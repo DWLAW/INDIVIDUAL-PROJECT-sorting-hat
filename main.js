37ffa8612b434847cdd7ac5a5ceae8f7e34a30ad
Dustin L <lawson.dustin12@gmail.com>
2024-04-27 10:40:27 -0500

    iterated arrays to the dom as cards

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
+// initial students array
+const students = [
+  {
+    id: 1,
+    name: "Harry",
+    house: "Gryffindor",
+    imageUrl:"img/harry.jpg",
+  },
+  {
+    id: 1,
+    name: "Droco",
+    house: "Slytherin",
+    imageUrl:"img/Malfoy.jpg",
 
-//TODO iterate the house cards to the DOM
-//TODOadd a delete button to the cards that adds the info into another array for voldemort
+  },
+];
+
+const badGuys = [
+{
+  id: 1,
+  name: "Voldemort",
+  house: "Voldemort's Army",
+  imageUrl:"img/Voldemor.jpg",
+},
+];
 
 
 //Utility function that renders to DOM
@@ -14,21 +36,11 @@ const renderToDom = (divId, textToRender) => {
 // TODO fill out data on card, add an image, etc.
 const introCardDom = () => {
   const domString = `
-  <div class="card" style="width: 18rem;">
+  <div class="card" style="width: 50rem;">
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
-  <h5 class="card-title">Card title</h5>
+  <h5 class="Welcome to Hogwarts Sorting Hat">Card title</h5>
   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  <ul class="list-group list-group-flush">
-  <li class="list-group-item">An item</li>
-  <li class="list-group-item">A second item</li>
-  <li class="list-group-item">A third item</li>
-  </ul>
-  <div class="card-body">
-  <a href="#" class="card-link">Card link</a>
-  <a href="#" class="card-link">Another link</a>
-  </div>
   </div>
   `;
   renderToDom("#introCard", domString)
@@ -47,7 +59,8 @@ const filterBtns = () => {
 `;
 renderToDom("#filterContainer", domString)
 };
-
+// form
+// TODO make form work
 const formModal = () => {
 const domString = `<!-- Button trigger modal -->
 <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#addStudent">
@@ -76,9 +89,50 @@ Apply to be a First Year Student!
 `;
 renderToDom('#formContainer', domString);
 };
+//TODO iterate the house cards to the DOM\
+const cardsOnDom = (students) => {
+  let domString = "";
+  for (const student of students) {
+    //card 
+      domString += `
+      <div id = "studentCardContainer"> 
+      <h4>Students</h4>
+        <div class="card" style="width: 18rem;" id = "studentCards">
+          <h5 class="card-title">${student.name}</h5>
+          <img src=${student.imageUrl} class="card-img-top" alt=${student.name}>
+          <div class="card-body">
+            <p class="card-text" id="${student.house}">${student.house}</p>
+            <button type="button" class="btn btn-danger"id="delete--${student.id}">Expel</button>
+          </div>
+        </div>
+       </div> 
+      `;
+      }
+    renderToDom('#cardContainer', domString);
+};
 
+const evilCardsOnDom = (badGuys) => {
+  let domString = "";
+  for (const badGuy of badGuys) {
+    //card 
+      domString += `
+      <div id = "evilCardContainer"> 
+      <h4>Army of Darkness</h4>
+        <div class="card" style="width: 18rem;" id = "badGuyCards">
+          <h5 class="card-title">${badGuy.name}</h5>
+          <img src=${badGuy.imageUrl} class="card-img-top" alt=${badGuy.name}>
+          <div class="card-body">
+            <p class="card-text" id="${badGuy.house}">${badGuy.house}</p>
+            
+          </div>
+        </div>
+       </div> 
+      `;
+      }
+    renderToDom('#evilCardContainer', domString);
+};
 
-
+// TODO event listerners 
 
 
 
@@ -88,6 +142,8 @@ renderToDom('#formContainer', domString);
     introCardDom();
     formModal();
     filterBtns();
+    cardsOnDom(students);
+    evilCardsOnDom(badGuys);
 
   };
 
